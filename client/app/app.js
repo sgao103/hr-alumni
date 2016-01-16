b2149741d0de95a650f9ea7298892d5f349e80ac
Brendan Grady <cyhtan@gmail.com>
2016-01-16 14:21:06 -0500

    Refactor states to use nested views

--- client/app/app.js
+++ client/app/app.js
@@ -3,39 +3,69 @@ angular.module('app', ['ui.router',
     'app.login',
     'app.profiles',
     'app.updateProfile',
-    'profileFactory',
-    'httpFactory',
     'jobPosting.factory',
+<<<<<<< HEAD
     'jobPosting.jobPostingPost.controller',
     'jobPosting.jobPostingSearch.controller',
     'jobPosting.jobPostingSpecific.controller'
+=======
+    'jobPosting.controller.jobPostingPost',
+    'jobPosting.controller.jobPostingSearch',
+    'jobPosting.controller.jobPostingSpecific',
+    'userFactory',
+    'profileFactory',
+    'httpFactory'
+>>>>>>> Refactor states to use nested views
 ])
+
+.run(function($rootScope, User, $state) {
+  $rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams) {
+    if (!User.isloggedIn() && toState.name !== 'app.login' && toState.name !== 'app.home') {
+      event.preventDefault();
+      // window.localStorage.setItem('userToken', ____ )
+      $state.go('app.login');
+    }
+  })
+})
 
 .config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
   $urlRouterProvider.otherwise('/home');
   $stateProvider
-    .state('home', {
-      url:'/home',
+
+    .state('app', {
+      url: '/',
       views: {
         'mainContent': {
+          templateUrl: 'app/home/home.html',
+          controller:  'HomeCtrl'
+        },
+        'header' :{
+          templateUrl: 'app/_partials/nav.html',
+        }
+      }
+    })
+    .state('app.home', {
+      url:'home',
+      views: {
+        'mainContent@': {
           templateUrl: 'app/home/home.html',
           controller:  'HomeCtrl'
         }
       }
     })
-    .state('login', {
-      url: '/login',
+    .state('app.login', {
+      url: 'login',
       views: {
-        'mainContent': {
+        'mainContent@': {
           templateUrl: 'app/login/login.html',
           controller:  'LoginCtrl'
         }
       }
     })
-    .state('profiles', {
-      url: '/profiles',
+    .state('app.profiles', {
+      url: 'profiles',
       views: {
-        'mainContent': {
+        'mainContent@': {
           templateUrl: 'app/profiles/profiles.html', // plural
           controller:  'ProfilesCtrl'
         }
@@ -54,10 +84,15 @@ angular.module('app', ['ui.router',
           controller:  'UpdateProfileCtrl'
         }
       }
+<<<<<<< HEAD
     })
     .state('jobPostings', {
+=======
+    })    
+    .state('app.jobPostings', {
+>>>>>>> Refactor states to use nested views
       views: {
-        'mainContent': {
+        'mainContent@': {
           templateUrl: 'app/jobPostings/jobPosting/jobPostings.html',
         }
       }
