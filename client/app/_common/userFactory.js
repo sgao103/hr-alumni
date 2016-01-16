8a351e24d50110007e2fe119a16a7fe80c9461dc
Brendan Grady <cyhtan@gmail.com>
2016-01-16 14:01:19 -0500

    Add userFactory for session handling

---
+++ client/app/_common/userFactory.js
@@ -0,0 +1,68 @@
+angular.module('userFactory', [])
+
+.factory('User', function($http, $state) {
+
+  var loggedIn = true;
+
+  var apiUrl = 'http://localhost:5007'
+
+  /*
+   * Sends login credentials from submit form to server
+   * Based on response, function enables login session and sets token in localStorage
+   * Setting historyRoot to true, creates a clean history path for current view
+   */
+
+  // var login = function(credentials) {
+  //   $http.post(apiUrl + '/api/user/login', credentials).then(function(response) {
+  //     if (response.data.id !== undefined) {
+  //       loggedIn = true;
+  //       window.localStorage.setItem('which.userToken', response.data.id);
+  //       $ionicHistory.nextViewOptions({
+  //         historyRoot: true
+  //       })
+  //       $state.go('app.tagView')
+  //     }
+  //   })
+  // };
+
+  var isloggedIn = function() {
+    return loggedIn;
+  }
+
+
+  /*
+   * Sends signUp credentials from submit form to server
+   */
+
+  // var signUp = function(credentials) {
+  //   $http.post(apiUrl + '/api/user/signup', credentials).then(function(response) {
+  //     if (response.data.id !== undefined) {
+  //       loggedIn = true;
+  //       window.localStorage['which.userToken'] = response.data.id;
+  //       $ionicHistory.nextViewOptions({
+  //         historyRoot: true
+  //       });
+  //       $state.go('app.tagView');
+  //     }
+  //   })
+  // };
+
+
+  /*
+   * Deletes current session on device
+   */
+
+  // var signOut = function() {
+  //   loggedIn = false;
+  //   window.localStorage.removeItem('which.userToken');
+  //   $state.go('app.login');
+  // };
+
+  return {
+    isloggedIn: isloggedIn,
+    // signUp: signUp,
+    // login: login,
+    // signOut: signOut
+  }
+
+});
