f19d45de55bc198593f441531d7682596057cd29
Mad Bernard <madeline.a.bernard@gmail.com>
2016-01-16 20:43:25 -0800

    Add checkbox and begin to build 'hireable' filter

--- client/app/profiles/profilesCtrl.js
+++ client/app/profiles/profilesCtrl.js
@@ -2,18 +2,41 @@ angular.module('profiles.controller', ['ui.router'])
 
 .controller('ProfilesCtrl', ['$scope', '$http', 'HttpRequest', 'Profile', function ($scope, $http, HttpRequest, Profile) {
 
-  HttpRequest.getProfiles()
-    .then(function (res) {
-      $scope.profiles = res.data;
-      $scope.setProfile= function (profile) {
-        $scope.currentProfile = Profile.setProfile(profile); 
-      }
-    });
+  HttpRequest.getProfiles().then(function (res) {
+    $scope.profiles = res.data;
+  });
 
-    // used for showing the modal in profiles.html
-    $scope.modalDetails = function(profile){
-        $scope.profile = profile;
-        $('#modalDetails').openModal();
-    };
+  $scope.hireable = {};
+    console.log(this.hireable, 'before anything');  // logs undefined "before anything"
 
-}])
+    // OMG checked toggles!
+  $scope.hireableFilter = function () {
+    console.log(this.hireable, 'before function');  // logs Object {checked: true} "before function"
+    this.hireable.value = true;
+    if (this.hireable.checked) {
+      console.log('box is checked');
+    }
+    else {
+      console.log('box is empty');
+    }
+    console.log(this.hireable, 'after function');  // logs Object {checked: true, value: true} "after function"
+    console.log($scope.hireable.checked.$modelValue);  // undefined
+  };
+
+  $scope.myVar = 1;
+
+  $scope.$watch('hireable', function() {
+      alert('hey, myVar has changed!');
+  });
+
+  $scope.buttonClicked = function() {
+     $scope.myVar = 2; // This will trigger $watch expression to kick in
+  };
+
+  // used for showing the modal in profiles.html
+  $scope.modalDetails = function(profile){
+    $scope.profile = profile;
+    $('#modalDetails').openModal();
+  };
+
+}]);
