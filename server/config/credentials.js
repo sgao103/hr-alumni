c0768a5f224631df806eb9e80b7fa4095661b8a4
veebuv <veebuv@gmail.com>
2016-01-19 03:00:29 +0400

    bower post install

--- server/config/credentials.js
+++ server/config/credentials.js
@@ -6,6 +6,6 @@ module.exports = {
 
     GITHUB_CLIENT_ID : "399d03e217b75d4f4625",
     GITHUB_CLIENT_SECRET : "8e4dcd1a7c66f1db0c438aa50aa3c4af8aa4d2d5",
-    GITHUB_CALLBACK_URL: "http://localhost:3000/auth/github/callback"
+    GITHUB_CALLBACK_URL: "http://teslalegacy.herokuapp.com/auth/github/callback"
 
 };
